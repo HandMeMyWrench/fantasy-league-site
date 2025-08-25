c39300b093e7b19fea484df5a963adae1e821876
Leigh Puckett <leighpuckett@Leighs-Mac-mini.local>
2025-08-25 11:18:02 -0400

    Removed unused variables for Vercel build

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react"
 import {
   getLeagueUsers,
   getStandings,
-  getLeagueMetadata,
+ // getLeagueMetadata,
 } from "@/lib/sleeper"
 
 type SeasonYear = "2025" | "2024"
